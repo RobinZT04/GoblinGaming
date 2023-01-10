7322a5e410e6f7d80fe0a9faee06377ef774527f
GoodGuyDeg <79506119+GoodGuyDeg@users.noreply.github.com>
2023-01-10 18:46:13 +0100

    Sleepy

---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition; //vector3 mouseposition blir musens positon
+
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //sätter mousepositonen sedd fr�n kameran
+
+        Vector2 direction = new Vector2 //Vector2 direction är new vector2
+        (
+        mousePosition.x - transform.position.x, //tar musens positon.x - transform.position.x
+        mousePosition.y - transform.position.y //tar musens positon.y - transform.position.y
+        );
+
+        transform.up = direction; //transform.up ör nu likamed direction
+    }
+}
--- Assets/SleepyMovement.cs
+++ Assets/SleepyMovement.cs
@@ -7,22 +7,41 @@ public class SleepyMovement : MonoBehaviour
 {
 
     public Transform player;
-
+    public float movementSpeed;
+    GameObject trash;
+    bool collecting;
     // Start is called before the first frame update
     void Start()
     {
-      
+        collecting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.transform.position) >= 3)
+        if (Vector2.Distance(transform.position, player.transform.position) >= 0.15f && !collecting)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 0.1f);
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 0.1f);
+            transform.position += (player.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime;
         }
 
+        if (GameObject.FindGameObjectWithTag("Trash") != null)
+        {
+            GameObject trash = GameObject.FindGameObjectWithTag("Trash");
+            collecting = true;
+
+            if(collecting)
+            transform.position += (trash.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime;
+
+            if (Vector2.Distance(transform.position, trash.transform.position) <= 0.15f)
+            {
+                Destroy(trash);
+                collecting = false;
+            }
+        
+        }
+            
             
     }
 }
