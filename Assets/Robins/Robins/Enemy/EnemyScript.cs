04285ddb7707b97c4d969a1c607a2f38dafb5c8f
Elanor <elanor.berg@elev.ga.lbs.se>
2023-01-12 11:23:40 +0100

    Enemies Path system

--- Assets/Robins/Robins/Enemy/EnemyScript.cs
+++ Assets/Robins/Robins/Enemy/EnemyScript.cs
@@ -12,21 +12,47 @@ public class EnemyScript : MonoBehaviour
     [SerializeField]
     HumanBody humanBody;
 
+    public Transform[] pathPos; //Elanor
+    public int nuvarandeposition; //Elanor
+    public bool chasing; //Elanor 
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        chasing = false;//Elanor
+        nuvarandeposition = 0; //Elanor
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, player.transform.position) <= 5)
+        if(Vector3.Distance(transform.position, player.transform.position) <= 5) //Elanor
         {
-            agent.SetDestination(new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z));
-            humanBody.Invoke("Head", 0);
+            chasing = true;
+        }
+        if (chasing) //Elanor
+        {
+            agent.SetDestination(new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z)); //Robin
+            humanBody.Invoke("Head", 0); //Robin 
+            chasing = true; //Elanor
         }
 
-        transform.eulerAngles = new Vector3(0, 0, 0);
+        transform.eulerAngles = new Vector3(0, 0, 0); //Robin
+
+        if (!chasing) //Elanor - Gubben rör sig mot nästa position när den har gått till sista positionen resetas det.
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pathPos[nuvarandeposition].transform.position, 1 * Time.deltaTime); //Elanor
+            if (Vector3.Distance(transform.position, pathPos[nuvarandeposition].transform.position) <= 0.2f) //Elanor
+            {
+                nuvarandeposition += 1; //Elanor
+            }
+
+            if (nuvarandeposition == pathPos.Length) //Elanor
+            {
+                nuvarandeposition = 0; //Elanor
+            }     
+          
+        }
     }
 }
